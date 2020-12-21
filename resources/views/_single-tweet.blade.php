93c18cf9d93620c9f1a8f0817b23e8f3c64da154
Artsiom Artsiomau <artem.artsemov@biggiko.com>
2020-12-21 17:59:51 +0300

    init project

---
+++ resources/views/_single-tweet.blade.php
@@ -0,0 +1,20 @@
+<div class="flex border-b border-b-gray-400">
+    <div class="m-4 flex-shrink-0">
+        <img
+            src="https://i.pravatar.cc/50"
+            alt=""
+            class="rounded-full mr-2"
+        >
+    </div>
+    <div>
+        <h5 class="font-bold mb-4 mt-2">John Doe</h5>
+
+        <p class="text-sm mb-2">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            Alias aspernatur facilis nisi porro quia voluptas? Aliquam amet
+            commodi delectus deserunt eos est, harum id incidunt, ipsa maiores
+            maxime minima natus nesciunt officiis, optio quis quo sapiente velit?
+            A ab accusamus accusantium aliquam aperiam blanditiis consectetur cum distinctio.
+        </p>
+    </div>
+</div>
